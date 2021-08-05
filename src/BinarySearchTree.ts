bf432cc0d43b56a6c49fac113b7d25dbf0222b7d
Eduardo Valencia <8025918@gmail.com>
2021-08-05 12:00:59 -0700

    Add initial insert function

--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -1,5 +1,7 @@
 import DataNode from "./Node";
 
+type Side = "left" | "right";
+
 class BinarySearchTree<Data> {
   private _head: DataNode<Data> | null;
 
@@ -31,6 +33,47 @@ class BinarySearchTree<Data> {
       yield this.traverse(startNode.right).next().value;
     }
   }
+
+  getIfShouldInsertOnSide = (
+    node: DataNode<Data>,
+    data: Data,
+    side: Side
+  ): boolean => {
+    const isGreaterThanCurrentData = data > node.data;
+    return side === "left"
+      ? !isGreaterThanCurrentData
+      : isGreaterThanCurrentData;
+  };
+
+  tryInsertingNodeOnSide = (
+    node: DataNode<Data>,
+    side: Side,
+    data: Data
+  ): void => {
+    const shouldInsertOnSide: boolean = this.getIfShouldInsertOnSide(
+      node,
+      data,
+      side
+    );
+    if (!shouldInsertOnSide) return;
+    if (!node[side]) {
+      node[side] = new DataNode<Data>(data);
+    }
+  };
+
+  insert = (data: Data, startNode: DataNode<Data> | null = this.head): void => {
+    if (!startNode) return;
+    else if (startNode.data === data) {
+      startNode.frequency++;
+    } else if (data < startNode.data && startNode.left) {
+      this.insert(data, startNode.left);
+    } else if (data > startNode.data && startNode.right) {
+      this.insert(data, startNode.right);
+    } else {
+      this.tryInsertingNodeOnSide(startNode, "left", data);
+      this.tryInsertingNodeOnSide(startNode, "right", data);
+    }
+  };
 }
 
 export default BinarySearchTree;
